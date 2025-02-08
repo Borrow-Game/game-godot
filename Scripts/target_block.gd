f06ed60de997c61f29b6c240c1bc31df3f00ffe5
Username4D <studio@kremzow.org>
2025-02-08 17:23:50 +0100

    feat: more haptic feedback

--- Scripts/target_block.gd
+++ Scripts/target_block.gd
@@ -8,3 +8,4 @@ extends StaticBody2D
 func _on_target_block_arrow_hitbox_body_entered(body: Node2D) -> void:
 	if body.is_in_group("arrow"):
 		triggered_note.trigger()
+		Input.start_joy_vibration(0, 0.5, 0.5, 0.1)
