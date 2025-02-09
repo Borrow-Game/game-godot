fff847d13da6477094ffd45b393606bab8c2bbb8
Username4D <studio@kremzow.org>
2025-02-09 21:47:44 +0100

    fix: various small bugfixes

--- Scripts/enemy_2d.gd
+++ Scripts/enemy_2d.gd
@@ -8,17 +8,16 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 		if body.active:
 			body.queue_free()
 			Input.start_joy_vibration(0, 0.1, 0.1, 0.2)
-			queue_free()
+			disable()
 	if body.is_in_group("player"):
 		RespawnHandler.respawn()
+		body.set_physics_process(false)
 		
 
 		
 func _physics_process(delta: float) -> void:
 
-	if RespawnHandler.respawning > 0:
-		self.position = startposition
-		speed = abs(speed)
+
 		
 	
 	
@@ -32,3 +31,15 @@ func _physics_process(delta: float) -> void:
 		speed  = abs(speed)
 	move_and_slide()	
 		
+func disable() -> void:
+	self.set_physics_process(false)
+	self.visible = false
+	self.position = Vector2(-99999, -99999)
+	
+	
+func _process(delta: float) -> void:
+		if RespawnHandler.respawning > 0:
+			self.position = startposition
+			speed = abs(speed)
+			self.set_physics_process(true)
+			self.visible = true
