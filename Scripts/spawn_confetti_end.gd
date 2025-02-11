1443ef3fb690ab00f40e66d12f5b4c5b9c051136
Liande Guo <liande@guocn.org>
2025-02-11 19:36:49 +0100

    feat: added confetti when finishing demo

---
+++ Scripts/spawn_confetti_end.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+var confetti_scene = preload('res://Scenes/confetti.tscn')
+
+func _on_body_entered(body):
+	if body.is_in_group("player"):  # Check if the player entered
+		spawn_confetti()  # Set your confetti position
+
+func spawn_confetti():
+	var instance = confetti_scene.instantiate()
+	instance.global_position = Vector2(1568, 32)  # Adjust the position as needed
+	# Add to the main scene (not the Area2D)
+	get_tree().current_scene.add_child(instance)
